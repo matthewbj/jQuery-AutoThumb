b4ff7fe183101a1ba4db5ed44143219785652117
Matthew Jordan <mbj755@gmail.com>
2012-08-08 11:47:13 -0400

    Final

--- src/jquery.autothumb.js
+++ src/jquery.autothumb.js
@@ -3,19 +3,24 @@
   Created by Matt Jordan
   Released under the CC BY-SA 3.0 License (http://creativecommons.org/licenses/by-sa/3.0/)  
 */
+/*
+  jQuery AutoThumb Plugin - https://github.com/matthewbj/jQuery-AutoThumb
+  Created by Matt Jordan
+  Released under the CC BY-SA 3.0 License (http://creativecommons.org/licenses/by-sa/3.0/)  
+*/
 (function ($) {
     $.fn.autoThumb = function (options) {
         var settings = $.extend({
             "maxWidth": 150,
             "maxHeight": 200,
-            "clickExpand": true
+            "lightbox": true
         }, options);
         return this.each(function () {
             var $this = $(this),
                 rat = 0,
                 width = $this.width(),
                 height = $this.height();
-            $this.data('return-size', {
+            $this.data('original-size', {
                 "width": width,
                 "height": height
             });
@@ -37,18 +42,47 @@
                 });
                 width = width * rat
             }
-            if (settings.clickExpand === true) {
-                $this.css('cursor', 'pointer').attr('title', 'Click me to expand/contract').on('click', function () {
-                    var dat = $this.data('return-size');
-                    $this.data('return-size', {
-                        "width": $this.width(),
-                        "height": $this.height()
-                    }).attr({
-                        "height": dat.height,
-                        "width": dat.width
-                    });
+            if (settings.lightbox !== false) {
+                $this.css('cursor', 'pointer').attr('title', 'View Larger').on('click', function () {
+                    _autoThumbModal($(this), width, height);
                 });
             }
         });
     };
+    var modalSet = false;
+
+    function _autoThumbModal($this, width, height) {
+        var dat = $this.data('original-size'),
+            win = $(window),
+            elHeight = dat.height,
+            elWidth = dat.width,
+            maxModalWidth = (win.width() - 25),
+            maxModalHeight = (win.height() - 75);
+        if (modalSet === false) {
+            modalSet = true;
+            $('<div id="autoThumb-modal" /><div id="autoThumb-modal-bg" />').appendTo('body');
+        }
+        if (elWidth > maxModalWidth) {
+            elHeight = elHeight * (maxModalWidth / elWidth);
+            elWidth = maxModalWidth;
+        }
+        if (elHeight > maxModalHeight) {
+            elHeight = maxModalHeight;
+            elWidth = width * (maxModalHeight / height);
+        }
+        var html = '<img src="' + $this.attr('src') + '" height="' + elHeight + '" width="' + elWidth + '" alt="Loading..." class="autoThumb-image"><div id="autoThumb-modal-close" />',
+            css = {
+                'width': elWidth,
+                'height': elHeight,
+                'margin-top': '-' + Math.round((elHeight / 2) + 6) + 'px',
+                'margin-left': '-' + Math.round((elWidth / 2) + 6) + 'px'
+            };
+        $('#autoThumb-modal-bg').fadeIn(400);
+        $('#autoThumb-modal').html(html).css(css).fadeIn(400);
+    }
+    $(document).on('click', '#autoThumb-modal-close, #autoThumb-modal-bg', function (e) {
+        $("[id^=autoThumb-modal]").fadeOut(400, function () {
+            $('#autoThumb-modal').html('');
+        });
+    });
 })(jQuery);
